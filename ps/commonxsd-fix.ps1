ef8cae1c8bbe86ee9fe6cedafab7ef85feee4187
mraley <michaeldraley@gmail.com>
2025-08-18 14:43:38 -0500

    update

--- ps/commonxsd-fix.ps1
+++ ps/commonxsd-fix.ps1
@@ -1,118 +1,139 @@
-# Path to your common schema
-$common = "C:\code\encore\encore-api-commons\host-comm-library\host-comm\common-types\src\main\resources\xsd\common.xsd"
-
-# 1) Load XML preserving whitespace
-[xml]$doc = New-Object System.Xml.XmlDocument
-$doc.PreserveWhitespace = $true
-$doc.Load($common)
-
-# 2) Ensure correct header
-$schema = $doc.DocumentElement
-$XSD_NS = "http://www.w3.org/2001/XMLSchema"
-if ($schema.LocalName -ne "schema" -or $schema.NamespaceURI -ne $XSD_NS) {
-  throw "Top element is not xsd:schema"
-}
-$schema.SetAttribute("targetNamespace", "http://arvest.com/Encore/Common")
-$schema.SetAttribute("elementFormDefault", "qualified")
-$schema.SetAttribute("attributeFormDefault", "unqualified")
-# xmlns:xsd is implicit from the element; make sure xmlns:tns exists
-if (-not $schema.HasAttribute("xmlns:tns")) {
-  $schema.SetAttribute("xmlns:tns", "http://arvest.com/Encore/Common")
-}
-
-# Remove self-imports if any
-$imports = $schema.SelectNodes("xsd:import[@namespace='http://arvest.com/Encore/Common']", (New-Object System.Xml.XmlNamespaceManager $doc.NameTable).tap({
-  $_.AddNamespace("xsd",$XSD_NS)
-}))
-if ($imports) { $imports | ForEach-Object { $schema.RemoveChild($_) | Out-Null } }
-
-# 3) Collect locally defined type names
-$nsm = New-Object System.Xml.XmlNamespaceManager($doc.NameTable)
-$nsm.AddNamespace("xsd",$XSD_NS)
-$localTypeNodes = @()
-$localTypeNodes += $schema.SelectNodes("xsd:simpleType[@name]", $nsm)
-$localTypeNodes += $schema.SelectNodes("xsd:complexType[@name]", $nsm)
-$localNames = [System.Collections.Generic.HashSet[string]]::new([StringComparer]::Ordinal)
-$null = $localTypeNodes | ForEach-Object { $localNames.Add($_.GetAttribute("name")) | Out-Null }
-
-# Helpers
-function Is-Builtin([string]$name) {
-  $builtins = @(
-    'anySimpleType','anyType','string','boolean','decimal','float','double','duration','dateTime','time','date',
-    'gYearMonth','gYear','gMonthDay','gDay','gMonth','hexBinary','base64Binary','anyURI','QName','NOTATION',
-    'normalizedString','token','language','Name','NCName','ID','IDREF','IDREFS','ENTITY','ENTITIES',
-    'integer','nonPositiveInteger','negativeInteger','long','int','short','byte','nonNegativeInteger',
-    'unsignedLong','unsignedInt','unsignedShort','unsignedByte','positiveInteger'
-  )
-  return $builtins -contains $name
-}
-
-# 4) Prefix @type and @base
-$changed = $false
-$attrPaths = @(
-  "//xsd:element[@type]",
-  "//xsd:attribute[@type]",
-  "//xsd:restriction[@base]",
-  "//xsd:extension[@base]",
-  "//xsd:list[@itemType]",
-  "//xsd:union[@memberTypes]"
+param(
+  # Path to your common schema
+  [Parameter(Mandatory=$true)][string]$Common = "C:\code\encore\schema\Common\common.xsd",
+  # Your common namespace URI
+  [string]$CommonNs = "http://arvest.com/Encore/Common"
 )
 
-foreach ($path in $attrPaths) {
-  $nodes = $schema.SelectNodes($path, $nsm)
-  foreach ($node in $nodes) {
-    foreach ($attrName in @("type","base","itemType","memberTypes")) {
-      if (-not $node.HasAttribute($attrName)) { continue }
-      $val = $node.GetAttribute($attrName).Trim()
-      if ([string]::IsNullOrEmpty($val)) { continue }
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
 
-      # Skip already prefixed
-      if ($val -match '^[A-Za-z_][\w\.\-]*:') { continue }
+# ---------- helpers ----------
+$XSD_NS = "http://www.w3.org/2001/XMLSchema"
+$BUILTINS = @(
+  'anySimpleType','string','boolean','decimal','float','double','duration',
+  'dateTime','time','date','gYearMonth','gYear','gMonthDay','gDay','gMonth',
+  'hexBinary','base64Binary','anyURI','QName','NOTATION',
+  'normalizedString','token','language','Name','NCName','ID','IDREF','IDREFS',
+  'ENTITY','ENTITIES','integer','nonPositiveInteger','negativeInteger',
+  'long','int','short','byte','nonNegativeInteger','unsignedLong',
+  'unsignedInt','unsignedShort','unsignedByte','positiveInteger'
+) | ForEach-Object { "xsd:$_" }
 
-      # union memberTypes can be space-separated; handle that case
-      $parts = $val -split '\s+'
-      $newParts = @()
-      $tweaked = $false
-      foreach ($p in $parts) {
-        if ($p -match '^[A-Za-z_][\w\.\-]*:') { $newParts += $p; continue }
-        if (Is-Builtin $p)   { $newParts += "xsd:$p"; $tweaked = $true; continue }
-        if ($localNames.Contains($p)) { $newParts += "tns:$p"; $tweaked = $true; continue }
-        # leave as-is for now (will be reported)
-        $newParts += $p
-      }
-      if ($tweaked) {
-        $node.SetAttribute($attrName, ($newParts -join ' '))
-        $changed = $true
-      }
-    }
-  }
+function Add-PrefixIfNeeded {
+  param([string]$raw)
+  if ([string]::IsNullOrWhiteSpace($raw)) { return $raw }
+
+  # already a QName?
+  if ($raw -match '^\w+:\w+$') { return $raw }
+
+  # xsd built-ins (no prefix yet -> give xsd:)
+  if ($BUILTINS -contains ("xsd:$raw")) { return "xsd:$raw" }
+
+  # otherwise assume it's a local/common type => c:
+  return "c:$raw"
 }
 
-if ($changed) { $doc.Save($common) }
+# ---------- load ----------
+[xml]$doc = New-Object System.Xml.XmlDocument
+$doc.PreserveWhitespace = $true
+$doc.Load($Common)
 
-# 5) Report any remaining unprefixed references (real problems)
+$schema = $doc.DocumentElement
+if (-not $schema -or $schema.LocalName -ne 'schema' -or $schema.NamespaceURI -ne $XSD_NS) {
+  throw "Top element is not xsd:schema in $Common"
+}
+
+# NS manager
+$nsm = New-Object System.Xml.XmlNamespaceManager($doc.NameTable)
+$nsm.AddNamespace('xsd', $XSD_NS)
+
+# ---------- normalize header ----------
+$schema.SetAttribute('targetNamespace', $CommonNs) | Out-Null
+$schema.SetAttribute('elementFormDefault', 'qualified')   | Out-Null
+$schema.SetAttribute('attributeFormDefault', 'unqualified') | Out-Null
+if (-not $schema.HasAttribute('xmlns:c'))   { $schema.SetAttribute('xmlns:c',   $CommonNs)   | Out-Null }
+if (-not $schema.HasAttribute('xmlns:tns')) { $schema.SetAttribute('xmlns:tns', $CommonNs)   | Out-Null }
+if (-not $schema.HasAttribute('xmlns:xsd')) { $schema.SetAttribute('xmlns:xsd', $XSD_NS)     | Out-Null }
+
+# ---------- remove self-import/include (common importing itself) ----------
+$imports = $schema.SelectNodes("//xsd:import[@namespace='$CommonNs']|//xsd:include", $nsm)
+$removed = 0
+foreach ($imp in @($imports)) {
+  # remove only if it points at our common or clearly self-include
+  $loc = $imp.GetAttribute('schemaLocation')
+  if ($imp.LocalName -eq 'include' -or
+      $imp.GetAttribute('namespace') -eq $CommonNs -or
+      ($loc -and ($loc -match '(?i)\\common\\common\.xsd$' -or $loc -match '(?i)/common/common\.xsd$'))) {
+    $imp.ParentNode.RemoveChild($imp) | Out-Null
+    $removed++
+  }
+}
+if ($removed -gt 0) { Write-Host "Removed $removed self-import/include node(s)." -ForegroundColor Yellow }
+
+# ---------- collect local type names (defined in common) ----------
+$localNames =
+  @($schema.SelectNodes('xsd:complexType[@name]', $nsm)) +
+  @($schema.SelectNodes('xsd:simpleType[@name]',  $nsm)) |
+  ForEach-Object { $_.GetAttribute('name') } |
+  Sort-Object -Unique
+
+# For quick membership test
+$localSet = @{}
+$localNames | ForEach-Object { $localSet[$_] = $true }
+
+# ---------- rewrite @type and @base everywhere ----------
+$changed = 0
+
+# attributes named "type" or "base" under the schema subtree
+$attrs = $schema.SelectNodes('.//@type | .//@base', $nsm)
+foreach ($a in $attrs) {
+  $val = $a.Value
+  if ([string]::IsNullOrWhiteSpace($val)) { continue }
+
+  # already qualified?
+  if ($val -match '^\w+:\w+$') { continue }
+
+  # choose prefix based on built-ins or local types
+  if ($BUILTINS -contains "xsd:$val") {
+    $new = "xsd:$val"
+  } elseif ($localSet.ContainsKey($val)) {
+    $new = "c:$val"
+  } else {
+    # unknown; still give c: so JAXB sees a QName, and we'll report it below
+    $new = "c:$val"
+  }
+
+  if ($new -ne $val) {
+    $a.Value = $new
+    $changed++
+  }
+}
+Write-Host "Prefixed $changed @type/@base attribute(s)." -ForegroundColor Cyan
+
+# ---------- find unresolved references (still not defined here or built-in) ----------
 $unresolved = @()
-foreach ($path in $attrPaths) {
-  $schema.SelectNodes($path, $nsm) | ForEach-Object {
-    foreach ($a in @("type","base","itemType","memberTypes")) {
-      if (-not $_.HasAttribute($a)) { continue }
-      ($_.GetAttribute($a) -split '\s+') | Where-Object {
-        $_ -and ($_ -notmatch '^[A-Za-z_][\w\.\-]*:') -and -not (Is-Builtin $_) -and -not ($localNames.Contains($_))
-      } | ForEach-Object {
-        $unresolved += [pscustomobject]@{
-          Attribute = $a
-          Value     = $_
-          Context   = $_.OuterXml
-        }
-      }
+
+$allRefs = $schema.SelectNodes('.//@type | .//@base', $nsm) | ForEach-Object { $_.Value } | Sort-Object -Unique
+foreach ($qname in $allRefs) {
+  if ($qname -match '^xsd:') { continue }
+  if ($qname -match '^(c|tns):(.+)$') {
+    $name = $Matches[2]
+    if (-not $localSet.ContainsKey($name)) {
+      $unresolved += $name
     }
   }
 }
+$unresolved = $unresolved | Sort-Object -Unique
 
+# ---------- save ----------
+$doc.Save($Common)
+Write-Host "Saved: $Common" -ForegroundColor Green
+
+# ---------- report ----------
 if ($unresolved.Count -gt 0) {
-  Write-Host ""
-  Write-Host "Unresolved references in common.xsd (need definitions or wrong names):" -ForegroundColor Yellow
-  $unresolved | Sort-Object Value -Unique | Format-Table -AutoSize
+  Write-Host "`nUnresolved references in common.xsd (add or fix these types):" -ForegroundColor Yellow
+  $unresolved | ForEach-Object { "  - $_" } | Write-Output
 } else {
-  Write-Host "common.xsd looks consistent: all references are prefixed and resolvable." -ForegroundColor Green
+  Write-Host "`nNo unresolved references detected in common.xsd." -ForegroundColor Green
 }
